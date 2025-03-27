7e909517f41f7c5bdf5b33052419f001ca309857
Abdul Rafay <abdullrrafay@gmail.com>
2025-03-27 06:21:21 +0500

    created add two number program

---
+++ Assignments 00 to 05/00_intro_python/01_add_two_numbers.py
@@ -0,0 +1,11 @@
+def main():
+    print("two numbers adding program")
+    num1 = input("Enter first number: ")
+    num1: int = int(num1)
+    num2 = input("Enter second number: ")
+    num2: int = int(num2)
+    sum = num1 + num2
+    print("Sum of two numbers is: ", sum)
+
+if __name__ == '__main__':
+    main()
