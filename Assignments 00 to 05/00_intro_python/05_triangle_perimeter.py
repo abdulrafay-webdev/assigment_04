d97be7822003fd0cae74486c9a075407c553ff0d
Abdul Rafay <abdullrrafay@gmail.com>
2025-03-27 06:41:17 +0500

    created 05_triangle_perimeter

---
+++ Assignments 00 to 05/00_intro_python/05_triangle_perimeter.py
@@ -0,0 +1,9 @@
+def main():
+    side1 = float(input("Enter the length of side 1: "))
+    side2 = float(input("Enter the length of side 2: "))
+    side3 = float(input("Enter the length of side 3: "))
+    parameter = side1 + side2 + side3
+    print("The perimeter of the triangle is: ", parameter)
+
+if __name__ == '__main__':
+    main()
